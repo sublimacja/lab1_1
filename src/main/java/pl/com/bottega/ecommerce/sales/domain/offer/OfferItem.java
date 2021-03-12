df8753ac92ecc128f7ec4f472bd6af57db5db2cb
sublimacja <zuzanna.adamiec@gmail.com>
2021-03-12 18:13:52 +0100

    dodanie klasy produkt

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -18,15 +18,7 @@ import java.util.Date;
 public class OfferItem {
 
     // product
-    private String productId;
-
     private BigDecimal productPrice;
-
-    private String productName;
-
-    private Date productSnapshotDate;
-
-    private String productType;
 
     private int quantity;
 
@@ -39,6 +31,8 @@ public class OfferItem {
 
     private BigDecimal discount;
 
+    private Product product;
+
     public OfferItem(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate,
             String productType, int quantity) {
         this(productId, productPrice, productName, productSnapshotDate, productType, quantity, null, null);
@@ -46,11 +40,8 @@ public class OfferItem {
 
     public OfferItem(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate,
             String productType, int quantity, BigDecimal discount, String discountCause) {
-        this.productId = productId;
         this.productPrice = productPrice;
-        this.productName = productName;
-        this.productSnapshotDate = productSnapshotDate;
-        this.productType = productType;
+        this.product = new Product(productId,productType,productName,productSnapshotDate);
 
         this.quantity = quantity;
         this.discount = discount;
@@ -64,24 +55,12 @@ public class OfferItem {
         this.totalCost = productPrice.multiply(new BigDecimal(quantity)).subtract(discountValue);
     }
 
-    public String getProductId() {
-        return productId;
+    public Product getProduct() {
+        return product;
     }
 
     public BigDecimal getProductPrice() {
         return productPrice;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public Date getProductSnapshotDate() {
-        return productSnapshotDate;
-    }
-
-    public String getProductType() {
-        return productType;
     }
 
     public BigDecimal getTotalCost() {
@@ -109,11 +88,9 @@ public class OfferItem {
         final int prime = 31;
         int result = 1;
         result = prime * result + (discount == null ? 0 : discount.hashCode());
-        result = prime * result + (productName == null ? 0 : productName.hashCode());
         result = prime * result + (productPrice == null ? 0 : productPrice.hashCode());
-        result = prime * result + (productId == null ? 0 : productId.hashCode());
-        result = prime * result + (productType == null ? 0 : productType.hashCode());
         result = prime * result + quantity;
+        result = prime * result + (product == null ? 0 : product.hashCode());
         result = prime * result + (totalCost == null ? 0 : totalCost.hashCode());
         return result;
     }
@@ -137,13 +114,15 @@ public class OfferItem {
         } else if (!discount.equals(other.discount)) {
             return false;
         }
-        if (productName == null) {
-            if (other.productName != null) {
+
+        if (product == null) {
+            if (other.product != null) {
                 return false;
             }
-        } else if (!productName.equals(other.productName)) {
+        } else if (!product.equals(other.product)) {
             return false;
         }
+
         if (productPrice == null) {
             if (other.productPrice != null) {
                 return false;
@@ -151,16 +130,7 @@ public class OfferItem {
         } else if (!productPrice.equals(other.productPrice)) {
             return false;
         }
-        if (productId == null) {
-            if (other.productId != null) {
-                return false;
-            }
-        } else if (!productId.equals(other.productId)) {
-            return false;
-        }
-        if (productType != other.productType) {
-            return false;
-        }
+
         if (quantity != other.quantity) {
             return false;
         }
@@ -182,28 +152,15 @@ public class OfferItem {
      * @return
      */
     public boolean sameAs(OfferItem other, double delta) {
-        if (productName == null) {
-            if (other.productName != null) {
-                return false;
-            }
-        } else if (!productName.equals(other.productName)) {
+        if (product.sameAs(other.product)){
             return false;
         }
+
         if (productPrice == null) {
             if (other.productPrice != null) {
                 return false;
             }
         } else if (!productPrice.equals(other.productPrice)) {
-            return false;
-        }
-        if (productId == null) {
-            if (other.productId != null) {
-                return false;
-            }
-        } else if (!productId.equals(other.productId)) {
-            return false;
-        }
-        if (productType != other.productType) {
             return false;
         }
 
---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,72 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Date;
+
+public class Product {
+
+    private String Id;
+    private String Type;
+    private String Name;
+    private Date SnapshotDate;
+
+    public Product(String id, String type, String name, Date snapshotDate) {
+        this.Id = id;
+        this.Type = type;
+        this.Name = name;
+        this.SnapshotDate = snapshotDate;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Date getSnapshotDate() {
+        return SnapshotDate;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (Name == null ? 0 : Name.hashCode());
+        result = prime * result + (Id == null ? 0 : Id.hashCode());
+        result = prime * result + (Type == null ? 0 : Type.hashCode());
+        return result;
+    }
+
+    public boolean sameAs(Product other) {
+        if (Name == null) {
+            if (other.Name != null) {
+                return false;
+            }
+        } else if (!Name.equals(other.Name)) {
+            return false;
+        }
+
+        if (Id == null) {
+            if (other.Id != null) {
+                return false;
+            }
+        } else if (!Id.equals(other.Id)) {
+            return false;
+        }
+
+        if (Type == null) {
+            if (other.Type != null) {
+                return false;
+            }
+        } else if (!Type.equals(other.Type)) {
+            return false;
+        }
+
+        return true;
+    }
+}
